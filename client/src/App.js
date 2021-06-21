c620f56e771e02d71eee91724d0252b5cf047303
Harshrgupta <harshrg0120@gmail.com>
2021-06-21 20:50:45 +0530

    Adding redirect for not found

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
-
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route,Redirect } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <Route path="/" exact component={Join} />
       <Route path="/chat" component={Chat} />
+      <Redirect to="/"></Redirect>
     </Router>
   );
 }
